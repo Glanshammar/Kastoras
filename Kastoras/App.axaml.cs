854e03ae54cd0bbf9ea75094e9ba088efb5e961b
Andreas Johansson <glanshammar70@gmail.com>
2025-02-11 14:27:25 +0100

    Added button icon class

--- Kastoras/App.axaml.cs
+++ Kastoras/App.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Kastoras.ViewModels;
@@ -32,4 +34,26 @@ public partial class App : Application
 
         base.OnFrameworkInitializationCompleted();
     }
+}
+
+public class ButtonExtensions : AvaloniaObject
+{
+    public static readonly AttachedProperty<IImage> IconSourceProperty =
+        AvaloniaProperty.RegisterAttached<ButtonExtensions, Button, IImage>("IconSource");
+
+    public static readonly AttachedProperty<bool> IsStretchedProperty =
+        AvaloniaProperty.RegisterAttached<ButtonExtensions, Button, bool>("IsStretched");
+
+    // Getters and setters
+    public static void SetIconSource(Button element, IImage value) 
+        => element.SetValue(IconSourceProperty, value);
+    
+    public static IImage GetIconSource(Button element) 
+        => element.GetValue(IconSourceProperty);
+    
+    public static void SetIsStretched(Button element, bool value) 
+        => element.SetValue(IsStretchedProperty, value);
+    
+    public static bool GetIsStretched(Button element) 
+        => element.GetValue(IsStretchedProperty);
 }
