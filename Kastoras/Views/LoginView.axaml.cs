e73513c8e024c2583484e0374badc26f820d9da8
Andreas Johansson <glanshammar70@gmail.com>
2025-02-13 22:00:26 +0100

    Added login code & asset

---
+++ Kastoras/Views/LoginView.axaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace Kastoras.Views;
+
+public partial class LoginView : UserControl
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+    
+    private void OnRegisterClick(object? sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("OnRegisterClick");
+    }
+}
