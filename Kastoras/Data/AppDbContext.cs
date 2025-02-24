b61e5eac5e228b409c919462af034146128ccc6f
Andreas Johansson <glanshammar70@gmail.com>
2025-02-24 18:14:35 +0100

    Added base DbContext

---
+++ Kastoras/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Kastoras.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kastoras.Data;
+
+public class AppDbContext : DbContext
+{
+    // public DbSet<Transaction> Transactions { get; set; }
+    // public DbSet<Category> Categories { get; set; }
+    // public DbSet<Budget> Budgets { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=kastaros.db");
+    }
+}
