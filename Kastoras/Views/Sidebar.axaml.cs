1ceb63b679cbe6aa9966c20eb08405195f0a1cad
Andreas Johansson <glanshammar70@gmail.com>
2025-02-25 17:32:30 +0100

    Added sidebar base

---
+++ Kastoras/Views/Sidebar.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Kastoras.Views;
+
+public partial class Sidebar : UserControl
+{
+    public Sidebar()
+    {
+        InitializeComponent();
+    }
+}
