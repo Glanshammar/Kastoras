b5ff9c3ceb33574a6b9883d944a438bb302f8f52
Andreas Johansson <glanshammar70@gmail.com>
2025-02-25 19:03:22 +0100

    Added IUserRepository

---
+++ Kastoras/Repos/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Kastoras.Models;
+
+namespace Kastoras.Repos;
+
+public interface IUserRepository
+{
+    Task AddUser(UserModel user);
+    Task<UserModel> GetUserByUsername(string username);
+}
